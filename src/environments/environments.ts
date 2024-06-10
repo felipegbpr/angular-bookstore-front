690b715df57de9b12a67b30fda0f1f195bb024f7
Felipe Borges <felipetecacc@gmail.com>
2024-06-10 15:14:15 -0300

    Categoria DELETE implementada

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://bookstoreapi-dac741a8e15a.herokuapp.com'
+    baseUrl: 'https://springboot-bookstore-api-2mvy.onrender.com'
 }; 
