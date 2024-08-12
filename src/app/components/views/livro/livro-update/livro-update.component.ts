c39a3a06c46a999093be979bd765f7e6b2883b3c
Felipe Borges <felipetecacc@gmail.com>
2024-08-11 23:58:05 -0300

    UPDATE para Livro implementado

--- src/app/components/views/livro/livro-update/livro-update.component.ts
+++ src/app/components/views/livro/livro-update/livro-update.component.ts
@@ -3,26 +3,25 @@ import { FormControl, Validators } from '@angular/forms';
 import { LivroService } from '../livro.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Livro } from '../livro.model.';
-
 @Component({
-  selector: 'app-livro-create',
-  templateUrl: './livro-create.component.html',
-  styleUrl: './livro-create.component.css'
+  selector: 'app-livro-update',
+  templateUrl: './livro-update.component.html',
+  styleUrl: './livro-update.component.css'
 })
-export class LivroCreateComponent implements OnInit {
-
-  id_cat: String = '';
+export class LivroUpdateComponent implements OnInit {
+  
+  id_cat: String = "";
 
   livro: Livro = {
-    id: '',
-    titulo: '',
-    nome_autor: '',
-    texto: ''
-  }
+    id: "",
+    titulo: "",
+    nome_autor: "",
+    texto: "",
+  };
 
-  titulo = new FormControl('', [Validators.minLength(3)]);
-  nome_autor = new FormControl('', [Validators.minLength(3)]);
-  texto = new FormControl('', [Validators.minLength(10)]);
+  titulo = new FormControl("", [Validators.minLength(3)]);
+  nome_autor = new FormControl("", [Validators.minLength(3)]);
+  texto = new FormControl("", [Validators.minLength(10)]);
 
   constructor(
     private service: LivroService,
@@ -31,21 +30,29 @@ export class LivroCreateComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!;
+    this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
+    this.livro.id = this.route.snapshot.paramMap.get("id")!;
+    this.findById()
   }
 
-  create(): void {
-    this.service.create(this.livro, this.id_cat).subscribe(resposta => {
+  cancel(): void {
+    this.router.navigate([`categorias/${this.id_cat}/livros`]);
+  }
+
+  findById(): void {
+    this.service.findById(this.livro.id!).subscribe((resposta) => {
+      this.livro = resposta
+    })
+  }
+
+  update():void {
+    this.service.update(this.livro).subscribe((resposta) => {
       this.router.navigate([`categorias/${this.id_cat}/livros`]);
-      this.service.mensagem('Livro criado com sucesso!');
+      this.service.mensagem('Livro atualizado com sucesso!')
     }, err => {
       this.router.navigate([`categorias/${this.id_cat}/livros`]);
-      this.service.mensagem('Erro ao criar novo livro. Tente mais tarde.');
-    });
-  }
-
-  cancel(): void{
-    this.router.navigate([`categorias/${this.id_cat}/livros`]);
+      this.service.mensagem('Falha ao atualizar livro! Tente mais tarde..')
+    })
   }
 
   getMessageTitulo() {
@@ -75,6 +82,4 @@ export class LivroCreateComponent implements OnInit {
     }
     return false;
   }
-
-
 }
