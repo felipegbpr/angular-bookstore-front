f44b9c76ff8e847d3c21ece45acc6cc2bc623c25
Felipe Borges <felipetecacc@gmail.com>
2024-09-01 19:03:46 -0300

    deploy

--- src/environments/environments-prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    baseUrl: 'https://springboot-bookstore-api-2mvy.onrender.com'
-}; 
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://springboot-bookstore-api-2mvy.onrender.com/'
+}; 
